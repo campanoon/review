65707e6b99addf5dc4ad7bc225e01d16acb6dcae
campanoon <jcampano@alumnos.unex.es>
2021-09-29 17:16:55 +0200

    First commit

--- src/main/java/com/udacity/jwdnd/c1/review/controller/ChatController.java
+++ src/main/java/com/udacity/jwdnd/c1/review/controller/ChatController.java
@@ -1,5 +1,4 @@
 package com.udacity.jwdnd.c1.review.controller;
-
 import com.udacity.jwdnd.c1.review.model.ChatForm;
 import com.udacity.jwdnd.c1.review.service.MessageService;
 import org.springframework.stereotype.Controller;
@@ -8,35 +7,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import java.util.List;
-
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
-
-    MessageService messageService;
-
+    private MessageService messageService;
     public ChatController(MessageService messageService) {
         this.messageService = messageService;
     }
-
-    @GetMapping()
-    public String getMessages(@ModelAttribute("chatForm") ChatForm chatForm, Model model){
-        model.addAttribute("messages",messageService.getMessageList().toString());
+    @GetMapping
+    public String getChatPage(@ModelAttribute("chatForm") ChatForm chatForm, Model model) {
+        model.addAttribute("chatMessages", this.messageService.getMessageList());
         return "chat";
     }
-
-    @PostMapping()
-    public String postMessage(@ModelAttribute("chatForm") ChatForm chatForm, Model model){
-        messageService.addMessage(chatForm);
+    @PostMapping
+    public String postChatMessage(@ModelAttribute("chatForm") ChatForm chatForm, Model model) {
+        this.messageService.addMessage(chatForm);
         chatForm.setMessageText("");
-        model.addAttribute("messages",messageService.getMessageList().toString());
+        model.addAttribute("chatMessages", this.messageService.getMessageList());
         return "chat";
     }
-
-    @ModelAttribute("allTypes")
-    public String[] populateTypes(){
-        return new String[] {"Say","Shout", "Whisper"};
+    @ModelAttribute("allMessageTypes")
+    public String[] allMessageTypes () {
+        return new String[] { "Say", "Shout", "Whisper" };
     }
 }
